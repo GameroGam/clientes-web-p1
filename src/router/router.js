167dc4d2c1619540b7ef45a895f8992c82b2c786
Gustavo Gamero <g.gustavogamero@gmail.com>
2025-10-02 19:07:53 -0300

    Adicción y configuración de VueRouter

---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from '../pages/Home.vue';
+import Register from '../pages/Register.vue';
+import Login from '../pages/Login.vue';
+import Profile from '../pages/Profile.vue';
+
+const routes = [
+
+    {path: '/',                  component: Home,},
+    {path: '/crear-cuenta',      component: Register,},
+    {path: '/iniciar-sesión',    component: Login,},
+    {path: '/perfil',            component: Profile,},
+
+];
